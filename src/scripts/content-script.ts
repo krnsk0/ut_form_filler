d0f9655d8b707748b5a8e03a8e8f047c3e5f6faa
Jonathan Kurinsky <jkurinsky@tubi.tv>
2023-12-03 11:26:56 -0500

    wip

--- src/scripts/content-script.ts
+++ src/scripts/content-script.ts
@@ -1,15 +1,55 @@
 import { createStore } from '../common/store/createRootStore';
 import { startStoreSync } from '../common/store/startStoreSync';
 import { makeLogger } from '../common/utils/makeLogger';
-import { injectMenu } from './injectMenu';
 
 const logger = makeLogger('content-script');
 logger.log('starting content script');
+
+const findQuestion = (questionNumber: number) => {
+  const questionNode = document.querySelectorAll('available-tests-list-item')[
+    questionNumber
+  ];
+  const surveyAmount = parseInt(
+    questionNode
+      ?.querySelector('.compensation-amount__text')
+      ?.textContent?.trim()
+      .replace('$', '') ?? '0',
+    10
+  );
+  const questionDiv: HTMLDivElement =
+    questionNode?.querySelector<HTMLDivElement>('.available-tests__tile')
+      ?.shadowRoot?.childNodes[0] as HTMLDivElement;
+  const slotDiv = questionDiv
+    ?.querySelector('slot')
+    ?.assignedNodes()[1] as HTMLDivElement;
+  const questionText = slotDiv
+    .querySelector<HTMLLegendElement>(
+      'screener-question .screener-question__title'
+    )
+    ?.innerText.trim();
+  const answers = (
+    slotDiv.querySelector('tk-single-select-list')?.shadowRoot
+      ?.childNodes[0] as HTMLDivElement
+  )?.querySelectorAll('.label-wrapper') as NodeListOf<HTMLDivElement>;
+
+  const parsedAnswers = Array.from(answers).map((answer: HTMLDivElement) => {
+    return {
+      select: () => answer.querySelector('input')?.click(),
+      answerText: answer.querySelector('label')?.innerText.trim(),
+    };
+  });
+
+  return {
+    surveyAmount,
+    questionText,
+    answers: parsedAnswers,
+  };
+};
 
 (async () => {
   const store = createStore();
   await startStoreSync(store);
   store.markLoadComplete();
 
-  injectMenu();
+  console.log('question', findQuestion(1));
 })();
